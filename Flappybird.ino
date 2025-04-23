1611797e75308c406d75cc34c45dac71e9caac20
MA0610 <47308923+MA0610@users.noreply.github.com>
2025-04-23 16:45:31 -0700

    made it so score can't go below 0

--- Flappybird.ino
+++ Flappybird.ino
@@ -151,7 +151,8 @@ void loop() {
   if (pipeX == 4) {
     if (birdPixel < pipeGapY || birdPixel > pipeGapY + 3) {
       // Crash: servo step back
-      score--;
+      if(score>0) {score--;}
+      else {score=0}
       servoPos -= servoStepBack;
       servoPos = constrain(servoPos, 0, 180);
       flapServo.write(servoPos);
